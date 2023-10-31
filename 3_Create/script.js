3f9e69039b971eddddeeebc899b5d0c7d1322d61
Sieglinde <sieglindevwb@gmail.com>
2023-10-31 15:15:40 +0100

    shuffle learners and adapt the text color to backgroundcolor

--- 3_Create/script.js
+++ 3_Create/script.js
@@ -3,33 +3,44 @@ const learners = ["Lucas", "Kilian", "Alec", "Mohamed", "Pieter", "Rebecca", "Al
 
 const currentArticle = document.querySelector('article');
 
-const hexCharacters = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+const shuffleLearner = learners.sort((a, b) => 0.5 - Math.random());
+
+const hexCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 function getCharacter(index) {
-	return hexCharacters[index]
+    return hexCharacters[index];
 }
 
 function generateNewColor() {
-	let hexColorRep = "#"
+    let hexColorRep = "#";
 
-	for (let index = 0; index < 6; index++){
-		const randomPosition = Math.floor ( Math.random() * hexCharacters.length ) 
-    	hexColorRep += getCharacter( randomPosition )
-	}
-	
-	return hexColorRep
+    for (let index = 0; index < 6; index++) {
+        const randomPosition = Math.floor(Math.random() * hexCharacters.length);
+        hexColorRep += getCharacter(randomPosition);
+    }
+
+    return hexColorRep;
 }
 
-//console.log( generateNewColor() ) 
+function getTextColor(hexColor) {
+    // Convert hex to RGB
+    const r = parseInt(hexColor.slice(1, 3), 16);
+    const g = parseInt(hexColor.slice(3, 5), 16);
+    const b = parseInt(hexColor.slice(5, 7), 16);
 
+    // Calculate luminance
+    const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
 
+    // Choose text color based on luminance
+    return luminance > 0.5 ? '#000000' : '#FFFFFF';
+}
 
-
-learners.forEach(function (learners) {
+learners.forEach(function (learner) {
     const newSection = document.createElement("section");
-    newSection.innerText = learners;
-    newSection.style.backgroundColor = generateNewColor();
-  
+    newSection.innerText = learner;
+    const bgColor = generateNewColor();
+    newSection.style.backgroundColor = bgColor;
+    newSection.style.color = getTextColor(bgColor);
     currentArticle.append(newSection);
-})
+});
 
