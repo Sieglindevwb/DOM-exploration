abb8573bd1898aba12fa489870d7b8eb14539d88
Sieglinde <sieglindevwb@gmail.com>
2023-10-31 14:28:27 +0100

    adding random colors to all sections

--- 3_Create/script.js
+++ 3_Create/script.js
@@ -1,2 +1,35 @@
-/*
-*/
+const learners = ["Lucas", "Kilian", "Alec", "Mohamed", "Pieter", "Rebecca", "Alessandro", "Josué", "Thibault", "Eduarda", "Mohammed", 
+                "Funda", "Luis", "Alexandru", "Andrej", "Danté", "jonasi", "Kelsey", "Rana", "Jana", "Sieglinde", "Gustave", "Sezin", "Thierry", "Sylvie", "Anaïs"]
+
+const currentArticle = document.querySelector('article');
+
+const hexCharacters = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+
+function getCharacter(index) {
+	return hexCharacters[index]
+}
+
+function generateNewColor() {
+	let hexColorRep = "#"
+
+	for (let index = 0; index < 6; index++){
+		const randomPosition = Math.floor ( Math.random() * hexCharacters.length ) 
+    	hexColorRep += getCharacter( randomPosition )
+	}
+	
+	return hexColorRep
+}
+
+//console.log( generateNewColor() ) 
+
+
+
+
+learners.forEach(function (learners) {
+    const newSection = document.createElement("section");
+    newSection.innerText = learners;
+    newSection.style.backgroundColor = generateNewColor();
+  
+    currentArticle.append(newSection);
+})
+
