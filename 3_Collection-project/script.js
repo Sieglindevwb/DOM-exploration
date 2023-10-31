9369c30a103989359bc0986c2be837c31096cb0d
Sieglinde <sieglindevwb@gmail.com>
2023-10-31 16:21:32 +0100

    add new excercies to git

---
+++ 3_Collection-project/script.js
@@ -0,0 +1,3 @@
+const listLike = ["Boardgames", "Music", "Clothes", "Furniture", "Games", "Series", "Streamers"]
+
+const SerieItems = ["GOT", "Sex Education", "New Girl", "Outlander", "Something in the rain", "You", "Breaking Bad", "Trailor Park Boys", "The Way of the Househusband", "The White Lotus"]
