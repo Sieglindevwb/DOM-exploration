eaf5d743585f1af1fb1d8ac1853cf869186afd5d
Sieglinde <sieglindevwb@gmail.com>
2023-10-31 17:52:04 +0100

    add h2 to second section

--- 4_Navigate/script.js
+++ 4_Navigate/script.js
@@ -6,3 +6,13 @@ const firstChild = ol.children[0]; // Select the first child of the <ol> tag
 ol.insertBefore(lastChild, firstChild);
 
 console.log(ol)
+
+const section = document.querySelectorAll('section');
+const h2 = document.querySelectorAll('h2');
+//console.log(section);
+const titleThird = section[1].querySelector('h2');
+const titleSecond = section[2].querySelector('h2');
+
+section[2].insertBefore(titleThird, section[2].firstElementChild);
+section[1].insertBefore(titleSecond, section[1].firstElementChild);
+console.log(section);
