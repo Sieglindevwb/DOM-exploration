bf1be996853fdd86ab629229042c8a058636cf84
Sieglinde <sieglindevwb@gmail.com>
2023-10-31 09:31:24 +0100

    add all paragraphs with a loop in the console

--- 2_Selectors/script.js
+++ 2_Selectors/script.js
@@ -1,13 +1,13 @@
 
 const allTitles = document.body.querySelectorAll(".important")
 
-
 allTitles.forEach (function (title) {
   title.setAttribute('title', 'This is an important item');
 });
 
 
 const importantImages = document.querySelectorAll('img.important');
+
 document.querySelectorAll('img').forEach(function (image) {
     if( Array.from(importantImages).includes(image)) {
         image.style.display = 'block';
@@ -16,3 +16,11 @@ document.querySelectorAll('img').forEach(function (image) {
     }
 });
 
+
+const allParagraphs = document.body.querySelectorAll("p");
+
+allParagraphs.forEach (function(paragraph){
+    console.log('Paragraph Text:' + paragraph.innerText);
+    console.log('Paragraph Class' + paragraph.className);
+    console.log('-----------------------');
+});
