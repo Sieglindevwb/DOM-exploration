4c670e349e4677893a20c344af1d39968fbea7da
Sieglinde <sieglindevwb@gmail.com>
2023-10-31 17:12:25 +0100

    last child to first place in ol

--- 4_Navigate/script.js
+++ 4_Navigate/script.js
@@ -1,2 +1,8 @@
-/*
-*/
+const ol = document.querySelector('ol');
+const lastChild = ol.children[4]; // Select the last child of the <ol> tag
+const firstChild = ol.children[0]; // Select the first child of the <ol> tag
+
+// Move the last child to the beginning of the list
+ol.insertBefore(lastChild, firstChild);
+
+console.log(ol)
