82eaa12e300afcb1544c36172b2dea8baa18b0ef
Sieglinde <sieglindevwb@gmail.com>
2023-11-06 09:54:16 +0100

    add properties to collections

--- 3_Collection-project/script.js
+++ 3_Collection-project/script.js
@@ -1,3 +1,85 @@
 const listLike = ["Boardgames", "Music", "Clothes", "Furniture", "Games", "Series", "Streamers"]
 
-const SerieItems = ["GOT", "Sex Education", "New Girl", "Outlander", "Something in the rain", "You", "Breaking Bad", "Trailor Park Boys", "The Way of the Househusband", "The White Lotus"]
+const Series = ["GOT", "Sex Education", "New Girl", "Outlander", "Something in the rain", "You", "Breaking Bad", "Trailor Park Boys", "The Way of the Househusband", "The White Lotus"]
+
+const collection = [ {
+    name: 'Game Of Thrones',
+    director: ['David Benioff',' D.B. Weiss'],
+    releaseDate: 'April 17, 2011',
+    picture: '',
+    genre: 'Fantasy',
+    cast: ['Emilia Clarke','Sophie Turner','Maisie Williams','Kit Harington','Lena Headey','Natalie Dormer','Pedro Pascal','Peter Dinklage']
+},
+{
+    name:'Sex Education',
+    director: 'Laurie Nunn',
+    releaseDate: 'Januari 11 2019',
+    picture:'', 
+    genre: ['comedy','drame'],
+    cast: ['Asa Butterfield','Emma Mackey','Ncuti Gatwa','Tanya Reynolds','Connor Swindells','Gillian Anderson','Mimi Keene','Patricia Allison']
+},
+{
+    name:'New Girl',
+    director:'Elizabeth Meriwether',
+    releaseDate:'September 20, 2011',
+    picture:'',
+    genre:'sitcom',
+    cast:['Zooey Deschanel','Jake Johnson','Max Greenfield','Hannah Simone','Lamorne Morris','Damon Wayans, Jr.','Nasim Pedrad','Julian Morris']
+},
+{
+    name:'Outlander',
+    director:'Diana Gabaldon',
+    releaseDate:'August 9, 2014',
+    picture:'',
+    genre:['drama','romantic'],
+    cast:['Sam Heughan','Caitriona Balfe','Richard Rankin','Sophie Skelton','Tobias Menzies','Lauren Lyle','John Bell','Duncan Lacroix']
+},
+{
+    name:'Something in the Rain',
+    director:'Ahn Pan-seok',
+    releaseDate:'March 30, 2018',
+    picture:'',
+    genre:['Romance', 'Drama'],
+    cast:['Son Ye-jin', 'Jung Hae-in', 'Jang So-yeon', 'Jang Won-hyung']
+},
+{
+    name:'You',
+    director:['Greg Berlanti', 'Sera Gamble'],
+    releaseDate:'September 9, 2018',
+    picture:'',
+    genre:['Thriller', 'Crime', 'Drama'],
+    cast:['Penn Badgley', 'Victoria Pedretti', 'Elizabeth Lail', 'Shay Mitchell']
+},
+{
+    name:'Breaking Bad',
+    director:'Vince Gilligan',
+    releaseDate:'January 20, 2008',
+    picture:'',
+    genre:[' Crime', 'Drama', 'Thriller'],
+    cast:['Bryan Cranston', 'Aaron Paul', 'Anna Gunn', 'Dean Norris', 'Betsy Brandt', 'RJ Mitte', 'Bob Odenkirk', 'Giancarlo Esposito']
+},
+{
+    name:'Trailer Park Boys',
+    director:['Mike Clattenburg', 'Robb Wells', 'John Paul Tremblay'],
+    releaseDate:'April 22, 2001',
+    picture:'',
+    genre:['Comedy', 'Crime'],
+    cast:['John Paul Tremblay', 'Robb Wells', 'Mike Smith', 'John Dunsworth', 'Patrick Roach']
+},
+{
+    name:'The Way of the Househusband',
+    director:'Kousuke Oono',
+    releaseDate:'April 8, 2021',
+    picture:'',
+    genre:['Comedy', 'Slice of Life'],
+    cast:['Kenjiro Tsuda', 'Shizuka Ito', 'Kazuyuki Okitsu', 'Yoshimasa Hosoya'],
+},
+{
+    name:'The White Lotus',
+    director:'Mike White',
+    releaseDate:'July 11, 2021',
+    picture:'',
+    genre:['Comedy', 'Drama'],
+    cast:['Murray Bartlett', 'Connie Britton', 'Jennifer Coolidge', 'Alexandra Daddario', 'Jake Lacy', 'Steve Zahn']
+}
+]
