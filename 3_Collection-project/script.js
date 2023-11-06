63ff6f2c11a0046f0efbe089ac9797fc845d824b
Sieglinde <sieglindevwb@gmail.com>
2023-11-06 11:44:58 +0100

    add HTML test to the file

--- 3_Collection-project/script.js
+++ 3_Collection-project/script.js
@@ -2,7 +2,8 @@ const listLike = ["Boardgames", "Music", "Clothes", "Furniture", "Games", "Serie
 
 const Series = ["GOT", "Sex Education", "New Girl", "Outlander", "Something in the rain", "You", "Breaking Bad", "Trailor Park Boys", "The Way of the Househusband", "The White Lotus"]
 
-const collection = [ {
+const seriesCollection = [ {
+    //first
     name: 'Game Of Thrones',
     director: ['David Benioff',' D.B. Weiss'],
     releaseDate: 'April 17, 2011',
@@ -10,22 +11,25 @@ const collection = [ {
     genre: 'Fantasy',
     cast: ['Emilia Clarke','Sophie Turner','Maisie Williams','Kit Harington','Lena Headey','Natalie Dormer','Pedro Pascal','Peter Dinklage']
 },
+    //second
 {
     name:'Sex Education',
     director: 'Laurie Nunn',
     releaseDate: 'Januari 11 2019',
-    picture:'', 
+    picture:'../img/sex_education_.jpg', 
     genre: ['comedy','drame'],
     cast: ['Asa Butterfield','Emma Mackey','Ncuti Gatwa','Tanya Reynolds','Connor Swindells','Gillian Anderson','Mimi Keene','Patricia Allison']
 },
+    //third
 {
     name:'New Girl',
     director:'Elizabeth Meriwether',
     releaseDate:'September 20, 2011',
     picture:'',
-    genre:'sitcom',
+    genre:['sitcom','comedy'],
     cast:['Zooey Deschanel','Jake Johnson','Max Greenfield','Hannah Simone','Lamorne Morris','Damon Wayans, Jr.','Nasim Pedrad','Julian Morris']
 },
+    //fourth
 {
     name:'Outlander',
     director:'Diana Gabaldon',
@@ -34,6 +38,7 @@ const collection = [ {
     genre:['drama','romantic'],
     cast:['Sam Heughan','Caitriona Balfe','Richard Rankin','Sophie Skelton','Tobias Menzies','Lauren Lyle','John Bell','Duncan Lacroix']
 },
+    //fifth
 {
     name:'Something in the Rain',
     director:'Ahn Pan-seok',
@@ -42,6 +47,7 @@ const collection = [ {
     genre:['Romance', 'Drama'],
     cast:['Son Ye-jin', 'Jung Hae-in', 'Jang So-yeon', 'Jang Won-hyung']
 },
+    //sixth
 {
     name:'You',
     director:['Greg Berlanti', 'Sera Gamble'],
@@ -50,6 +56,7 @@ const collection = [ {
     genre:['Thriller', 'Crime', 'Drama'],
     cast:['Penn Badgley', 'Victoria Pedretti', 'Elizabeth Lail', 'Shay Mitchell']
 },
+    //seventh
 {
     name:'Breaking Bad',
     director:'Vince Gilligan',
@@ -58,6 +65,7 @@ const collection = [ {
     genre:[' Crime', 'Drama', 'Thriller'],
     cast:['Bryan Cranston', 'Aaron Paul', 'Anna Gunn', 'Dean Norris', 'Betsy Brandt', 'RJ Mitte', 'Bob Odenkirk', 'Giancarlo Esposito']
 },
+    //eight
 {
     name:'Trailer Park Boys',
     director:['Mike Clattenburg', 'Robb Wells', 'John Paul Tremblay'],
@@ -66,6 +74,7 @@ const collection = [ {
     genre:['Comedy', 'Crime'],
     cast:['John Paul Tremblay', 'Robb Wells', 'Mike Smith', 'John Dunsworth', 'Patrick Roach']
 },
+    //nine
 {
     name:'The Way of the Househusband',
     director:'Kousuke Oono',
@@ -74,6 +83,7 @@ const collection = [ {
     genre:['Comedy', 'Slice of Life'],
     cast:['Kenjiro Tsuda', 'Shizuka Ito', 'Kazuyuki Okitsu', 'Yoshimasa Hosoya'],
 },
+    //ten
 {
     name:'The White Lotus',
     director:'Mike White',
@@ -83,3 +93,42 @@ const collection = [ {
     cast:['Murray Bartlett', 'Connie Britton', 'Jennifer Coolidge', 'Alexandra Daddario', 'Jake Lacy', 'Steve Zahn']
 }
 ]
+
+seriesCollection.forEach(series => {
+    const card = document.createElement('div');
+    card.className ='card';
+
+    const img = document.createElement('img');
+    img.src = series.picture;
+    img.alt = series.name;
+    img.className = 'card-image';
+
+    const cardInfo = document.createElement('div');
+    cardInfo.className = 'card-info';
+
+    const title = document.createElement('h2');
+    title.textContent = series.name;
+
+    const director = document.createElement('p');
+    director.innerHTML = `<strong>Director(s):</strong> ${series.director.join(', ')}`;
+
+    const releaseDate = document.createElement('p');
+    releaseDate.innerHTML = `<strong>Release Date:</strong> ${series.releaseDate}`;
+
+    const genre = document.createElement('p');
+    genre.innerHTML = `<strong>Genre(s):</strong> ${series.genre.join(', ')}`;
+
+    const cast = document.createElement('p');
+    cast.innerHTML = `<strong>Cast:</strong> ${series.cast.join(', ')}`;
+
+    cardInfo.appendChild(title);
+    cardInfo.appendChild(director);
+    cardInfo.appendChild(releaseDate);
+    cardInfo.appendChild(genre);
+    cardInfo.appendChild(cast);
+
+    card.appendChild(img);
+    card.appendChild(cardInfo);
+
+    cardContainer.appendChild(card);
+});
