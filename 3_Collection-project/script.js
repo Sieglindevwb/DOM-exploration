952df8bf6f7ff63ef02c40ccc9946d8fcb043623
Sieglinde <sieglindevwb@gmail.com>
2023-11-06 15:23:37 +0100

    delete html structure and add javacript

--- 3_Collection-project/script.js
+++ 3_Collection-project/script.js
@@ -1,4 +1,4 @@
-const listLike = ["Boardgames", "Music", "Clothes", "Furniture", "Games", "Series", "Streamers"]
+const cardContainer = document.querySelector('.card-conatiner');
 
 const Series = ["GOT", "Sex Education", "New Girl", "Outlander", "Something in the rain", "You", "Breaking Bad", "Trailor Park Boys", "The Way of the Househusband", "The White Lotus"]
 
